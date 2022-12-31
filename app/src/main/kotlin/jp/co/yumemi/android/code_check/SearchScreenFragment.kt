79850fa9d45b9cd3d8fce106e7f46c52dc416813
Kotaro Kamashima <kotaro0726@gmail.com>
2022-12-31 18:20:53 +0900

    ISSUE #1 変更: 【命名規則】SearchScreenFragment から `Local variable name '_xxxx' should start with a lowercase letter` の対応をしました

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchScreenFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchScreenFragment.kt
@@ -19,25 +19,25 @@ class SearchScreenFragment : Fragment(R.layout.fragment_search_screen) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val _binding = FragmentSearchScreenBinding.bind(view)
+        val binding = FragmentSearchScreenBinding.bind(view)
 
-        val _viewModel = SearchScreenViewModel(context!!)
+        val viewModel = SearchScreenViewModel(context!!)
 
-        val _layoutManager = LinearLayoutManager(context!!)
-        val _dividerItemDecoration =
-            DividerItemDecoration(context!!, _layoutManager.orientation)
-        val _adapter = CustomAdapter(object : CustomAdapter.OnItemClickListener {
+        val layoutManager = LinearLayoutManager(context!!)
+        val dividerItemDecoration =
+            DividerItemDecoration(context!!, layoutManager.orientation)
+        val adapter = CustomAdapter(object : CustomAdapter.OnItemClickListener {
             override fun itemClick(item: item) {
                 gotoRepositoryFragment(item)
             }
         })
 
-        _binding.searchInputText
+        binding.searchInputText
             .setOnEditorActionListener { editText, action, _ ->
                 if (action == EditorInfo.IME_ACTION_SEARCH) {
                     editText.text.toString().let {
-                        _viewModel.searchResults(it).apply {
-                            _adapter.submitList(this)
+                        viewModel.searchResults(it).apply {
+                            adapter.submitList(this)
                         }
                     }
                     return@setOnEditorActionListener true
@@ -45,17 +45,17 @@ class SearchScreenFragment : Fragment(R.layout.fragment_search_screen) {
                 return@setOnEditorActionListener false
             }
 
-        _binding.recyclerView.also {
-            it.layoutManager = _layoutManager
-            it.addItemDecoration(_dividerItemDecoration)
-            it.adapter = _adapter
+        binding.recyclerView.also {
+            it.layoutManager = layoutManager
+            it.addItemDecoration(dividerItemDecoration)
+            it.adapter = adapter
         }
     }
 
     fun gotoRepositoryFragment(item: item) {
-        val _action = SearchScreenFragmentDirections
+        val action = SearchScreenFragmentDirections
             .actionRepositoriesFragmentToRepositoryFragment(item = item)
-        findNavController().navigate(_action)
+        findNavController().navigate(action)
     }
 }
 
@@ -81,18 +81,18 @@ class CustomAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val _view = LayoutInflater.from(parent.context)
+        val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.layout_item, parent, false)
-        return ViewHolder(_view)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val _item = getItem(position)
+        val item = getItem(position)
         (holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView).text =
-            _item.name
+            item.name
 
         holder.itemView.setOnClickListener {
-            itemClickListener.itemClick(_item)
+            itemClickListener.itemClick(item)
         }
     }
 }
