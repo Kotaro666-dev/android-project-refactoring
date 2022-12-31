a8f1c0c7e88f5f2ce7f872f87308038c63e069df
Kotaro Kamashima <kotaro0726@gmail.com>
2022-12-31 17:44:26 +0900

    ISSUE #1 変更: 【命名規則】TopActivity から MainActivity に変更しました
    背景: 今回のプロジェクトでは Single Activity で開発されており、トップページを意味する命名よりもデフォルト時の命名 MainActivity に戻したほうが良いと判断しました

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/TwoFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/TwoFragment.kt
@@ -9,7 +9,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
-import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
+import jp.co.yumemi.android.code_check.MainActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.code_check.databinding.FragmentTwoBinding
 
 class TwoFragment : Fragment(R.layout.fragment_two) {
