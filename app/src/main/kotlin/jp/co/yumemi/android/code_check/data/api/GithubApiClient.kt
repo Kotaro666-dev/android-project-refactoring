4d1f9205216c3a2edf174c596e6fa68dfe5a2f41
Kotaro Kamashima <kotaro0726@gmail.com>
2023-01-03 11:19:52 +0900

    ISSUE #6 更新: Github への API リクエストする責務を持つ GithubApiClient を作成しました。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/api/GithubApiClient.kt
@@ -0,0 +1,33 @@
+package jp.co.yumemi.android.code_check.data.api
+
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GithubApiClient @Inject constructor() {
+    private val client = HttpClient(Android)
+
+    suspend fun getRepositories(searchKeyword: String): HttpResponse {
+        return withContext(Dispatchers.IO) {
+            client.get(GITHUB_SEARCH_API_ENDPOINT) {
+                header(
+                    "Accept",
+                    GITHUB_SEARCH_API_HEADER_ACCEPT_VALUE
+                )
+                parameter("q", searchKeyword)
+            }
+        }
+    }
+
+    companion object {
+        const val GITHUB_SEARCH_API_ENDPOINT = "https://api.github.com/search/repositories"
+        const val GITHUB_SEARCH_API_HEADER_ACCEPT_VALUE = "application/vnd.github.v3+json"
+    }
+}
+
